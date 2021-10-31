03850af41b6c041f7b16302192f4c8887300841e
waad <iwaad1414@gmail.com>
2021-10-31 13:25:45 +0300

    3 commit

--- app/src/main/java/com/example/todo/view/AddFragment.kt
+++ app/src/main/java/com/example/todo/view/AddFragment.kt
@@ -21,7 +21,7 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_add, container, false)
     }
 
