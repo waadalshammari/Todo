e32fb86acd674a235c801b7d28f9ad37387db1c6
waad <iwaad1414@gmail.com>
2021-11-01 13:32:32 +0300

    9 commit

--- app/src/main/java/com/example/todo/view/EditFragment.kt
+++ app/src/main/java/com/example/todo/view/EditFragment.kt
@@ -1,5 +1,6 @@
 package com.example.todolistapp.view
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,6 +11,7 @@ import android.widget.EditText
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.todo.R
+import java.util.*
 
 
 class EditFragment : Fragment() {
@@ -39,4 +41,23 @@ class EditFragment : Fragment() {
         todolistViewModel.addItem(title,des, false ,date)
 
     }
-} }
+        dateEditText.setOnClickListener {
+            // var fragment: DialogFragment = DatePickerDialog()
+            val getdate = Calendar.getInstance()
+            val datepicker = activity?.let { it1 ->
+                DatePickerDialog(
+                    it1,android.R.style.Theme_Holo_Dialog_NoActionBar_MinWidth,
+                    DatePickerDialog.OnDateSetListener { datePicker, i, i2, i3 ->
+                        val selectdate : Calendar = Calendar.getInstance()
+                        selectdate.set(Calendar.YEAR,i)
+                        selectdate.set(Calendar.MONTH,i2)
+                        selectdate.set(Calendar.DAY_OF_MONTH,i3)
+                        val dateEdit : String =
+
+                    }, getdate.get(Calendar.YEAR) , getdate.get(Calendar.MONTH) , getdate.get(
+                        Calendar.DAY_OF_MONTH))
+            }
+            datepicker!!.show()
+        }
+    }
+}
